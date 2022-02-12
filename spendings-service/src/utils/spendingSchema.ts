7713ca560061d881e240955423e59bec9f2c8b74
Mateusz Janduła <71319308+mj426382@users.noreply.github.com>
2022-02-12 15:51:37 +0100

    Add fully component (#2)
    Co-authored-by: mateusz janduła <mateusz.jandula@imagetemplates.com>

---
+++ spendings-service/src/utils/spendingSchema.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const newCarSchema = Joi.object({
+  name: Joi.string().required(),
+  cost: Joi.number().required(),
+  carId: Joi.string().required(),
+  date: Joi.date().required(),
+});
+
+export const validateAddSpending = (spending) => {
+  return newCarSchema.validate(spending);
+}
+
+const updateCarSchema = Joi.object({
+  _id: Joi.string().required(),
+  name: Joi.string().optional(),
+  cost: Joi.number().optional(),
+  carId: Joi.string().optional(),
+  date: Joi.date().optional(),
+});
+
+export const validateUpdateSpending = (spending) => {
+  return updateCarSchema.validate(spending);
+}
