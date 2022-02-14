0d183fca34a10a7b3e9afca257fc1c9679ff0e8a
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-02-14 01:43:48 +0100

    fix

--- mobile-app/src/App.js
+++ mobile-app/src/App.js
@@ -1,10 +1,15 @@
 import 'react-native-gesture-handler'
-import React from 'react'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/lib/integration/react'
-import { store, persistor } from '@/Store'
-import { ApplicationNavigator } from '@/Navigators'
 import './Translations'
+
+import { persistor, store } from '@/Store'
+
+import { ApplicationNavigator } from '@/Navigators'
+import { LogBox } from 'react-native'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { Provider } from 'react-redux'
+import React from 'react'
+
+LogBox.ignoreAllLogs(true)
 
 const App = () => (
   <Provider store={store}>
